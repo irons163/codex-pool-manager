01a5508f19346edf92affbf0290e1f45e2b6078c
Phil <irons7654321@gmail.com>
2026-03-27 06:57:05 +0800

    Inline account form add output construction

--- Features/PoolDashboard/PoolDashboardAccountFormFlowCoordinator.swift
+++ Features/PoolDashboard/PoolDashboardAccountFormFlowCoordinator.swift
@@ -18,13 +18,6 @@ struct PoolDashboardAccountFormFlowCoordinator {
         let nextState = actionFlowCoordinator.addAccount(to: state, name: name, quota: quota)
         var nextFormState = formState
         nextFormState.resetNewAccountInput(defaultQuota: defaultQuota)
-        return makeAddAccountOutput(state: nextState, formState: nextFormState)
-    }
-
-    private func makeAddAccountOutput(
-        state: AccountPoolState,
-        formState: PoolDashboardFormState
-    ) -> AddAccountOutput {
-        AddAccountOutput(state: state, formState: formState)
+        return AddAccountOutput(state: nextState, formState: nextFormState)
     }
 }
