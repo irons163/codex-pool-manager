3e435f8a78f9cbbec0c747d8383ce46313fed1a5
Phil <irons7654321@gmail.com>
2026-03-27 09:21:25 +0800

    Use menu picker style for manual account selection

--- Features/PoolDashboard/Components/StrategySettingsPanelView.swift
+++ Features/PoolDashboard/Components/StrategySettingsPanelView.swift
@@ -69,6 +69,7 @@ struct StrategySettingsPanelView: View {
                     Text(account.name).tag(account.id)
                 }
             }
+            .pickerStyle(.menu)
         }
     }
 }
