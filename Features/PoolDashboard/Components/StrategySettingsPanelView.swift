0d30ea6c0b07c370c3ea38926debe08ec3e53011
Phil <irons7654321@gmail.com>
2026-03-27 10:33:50 +0800

    Loosen strategy settings stack spacing further

--- Features/PoolDashboard/Components/StrategySettingsPanelView.swift
+++ Features/PoolDashboard/Components/StrategySettingsPanelView.swift
@@ -28,7 +28,7 @@ struct StrategySettingsPanelView: View {
         )
 
         GroupBox("策略設定") {
-            VStack(alignment: .leading, spacing: 14) {
+            VStack(alignment: .leading, spacing: 16) {
                 Stepper(
                     "最小切換間隔 \(Int(minSwitchIntervalBinding.wrappedValue)) 秒",
                     value: minSwitchIntervalBinding,
