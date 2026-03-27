30c20a1174fa648955d57b95e8cfca948460b73e
Phil <irons7654321@gmail.com>
2026-03-27 08:56:17 +0800

    Extract account form add output builder

--- Features/PoolDashboard/PoolDashboardAccountFormFlowCoordinator.swift
+++ Features/PoolDashboard/PoolDashboardAccountFormFlowCoordinator.swift
@@ -18,6 +18,13 @@ struct PoolDashboardAccountFormFlowCoordinator {
         let nextState = actionFlowCoordinator.addAccount(to: state, name: name, quota: quota)
         var nextFormState = formState
         nextFormState.resetNewAccountInput(defaultQuota: defaultQuota)
-        return AddAccountOutput(state: nextState, formState: nextFormState)
+        return makeAddAccountOutput(state: nextState, formState: nextFormState)
+    }
+
+    private func makeAddAccountOutput(
+        state: AccountPoolState,
+        formState: PoolDashboardFormState
+    ) -> AddAccountOutput {
+        AddAccountOutput(state: state, formState: formState)
     }
 }
