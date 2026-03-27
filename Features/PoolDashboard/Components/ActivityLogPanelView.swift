e3b82c900db67d99852557d66661bfe203b6fb7f
Phil <irons7654321@gmail.com>
2026-03-27 10:29:24 +0800

    Use tokenized min height in activity list

--- Features/PoolDashboard/Components/ActivityLogPanelView.swift
+++ Features/PoolDashboard/Components/ActivityLogPanelView.swift
@@ -38,7 +38,7 @@ struct ActivityLogPanelView: View {
                 }
                 .listStyle(.plain)
                 .scrollContentBackground(.hidden)
-                .frame(minHeight: 200)
+                .frame(minHeight: PoolDashboardTheme.activityListMinHeight)
             }
         }
         .sectionCardStyle()
