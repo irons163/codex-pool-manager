714dade370063a49d847dbec06dcc8e54c4efb65
Phil <irons7654321@gmail.com>
2026-03-27 10:05:55 +0800

    Increase strategy panel control spacing

--- Features/PoolDashboard/Components/StrategySettingsPanelView.swift
+++ Features/PoolDashboard/Components/StrategySettingsPanelView.swift
@@ -28,7 +28,7 @@ struct StrategySettingsPanelView: View {
         )
 
         GroupBox("策略設定") {
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 12) {
                 Stepper(
                     "最小切換間隔 \(Int(minSwitchIntervalBinding.wrappedValue)) 秒",
                     value: minSwitchIntervalBinding,
