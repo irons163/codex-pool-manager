760a66c742b9cf31177aac01710ffcefbec03737
Phil <irons7654321@gmail.com>
2026-03-27 10:32:41 +0800

    Add token for account add-row spacing

--- Features/PoolDashboard/PoolDashboardTheme.swift
+++ Features/PoolDashboard/PoolDashboardTheme.swift
@@ -71,6 +71,7 @@ enum PoolDashboardTheme {
     static let activityListMinHeight: CGFloat = 200
     static let backupEditorMinHeight: CGFloat = 190
     static let debugEditorMinHeight: CGFloat = 140
+    static let accountAddRowSpacing: CGFloat = 14
 }
 
 private struct SectionCardStyle: ViewModifier {
