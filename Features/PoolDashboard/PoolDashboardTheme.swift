2f495e2c4040953e8b90d85bc9b8812b91958f05
Phil <irons7654321@gmail.com>
2026-03-27 10:32:10 +0800

    Add token for header tile vertical padding

--- Features/PoolDashboard/PoolDashboardTheme.swift
+++ Features/PoolDashboard/PoolDashboardTheme.swift
@@ -66,6 +66,7 @@ enum PoolDashboardTheme {
     static let panelGlowShadowRadius: CGFloat = 20
     static let headerAccentRuleWidth: CGFloat = 180
     static let tileShadowRadius: CGFloat = 10
+    static let headerTileVerticalPadding: CGFloat = 11
     static let usageListMinHeight: CGFloat = 260
     static let activityListMinHeight: CGFloat = 200
     static let backupEditorMinHeight: CGFloat = 190
